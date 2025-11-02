2fbc17d09e018a940be6ec7032321657e9e0cdad
Hugo Frely <hugofrely@Hugos-MacBook-Pro.local>
2025-11-02 10:11:45 +0100

    update dockerfile api

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,13 +3,6 @@
 # Exit on error
 set -e
 
-echo "Waiting for database..."
-# Wait for database to be ready
-while ! nc -z db 5432; do
-  sleep 0.1
-done
-echo "Database is ready!"
-
 echo "Running migrations..."
 python manage.py migrate --noinput
 
